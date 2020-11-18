de6376aa9cecbd6d1a5f8e666806ca049ce8b87e
r3bunker <r3bunker@gmail.com>
2020-11-18 16:29:58 -0700

    basic c# projects

--- Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -5,8 +5,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to Acme Accouting Systems\n" +
-            "Remember, we're \"accouting\" on you!");
+        Console.WriteLine("What do you mean,\"escape\"?");
         Console.ReadLine();
     }
 }
