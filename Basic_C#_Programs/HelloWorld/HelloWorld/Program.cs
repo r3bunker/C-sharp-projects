17483f2254fd79fb9f9b86d0a6e4001e55ece9e6
r3bunker <r3bunker@gmail.com>
2020-11-18 15:56:56 -0700

    Create Program.cs

---
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accouting Systems\n" +
+            "Remember, we're \"accouting\" on you!");
+        Console.ReadLine();
+    }
+}
+
