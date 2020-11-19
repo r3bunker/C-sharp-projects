fc4f130a03ce890cb6d2a1d80e9b5bd01071abd0
r3bunker <r3bunker@gmail.com>
2020-11-19 11:00:14 -0700

    Drill 168

---
+++ 168_RentalCar/168_RentalCar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _168_RentalCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask for age
+            Console.WriteLine("What is your age?");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+
+            // Ask for DUI
+            Console.WriteLine("\nHave you ever had a DUI? (true or false)");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            // Speeding tickets
+            Console.WriteLine("\nHow many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            // Qualifications
+            Console.WriteLine("\nQualified?");
+            bool isQualified = (yourAge > 15 && DUI == false && speedingTickets < 3);
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}
