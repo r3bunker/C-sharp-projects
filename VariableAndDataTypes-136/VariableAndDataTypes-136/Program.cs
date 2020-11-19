12d73d2e217e0862cd41e4e5bf0d0679fcebbf7b
r3bunker <r3bunker@gmail.com>
2020-11-18 17:03:34 -0700

    VariablesAndDataTypes-136

---
+++ VariableAndDataTypes-136/VariableAndDataTypes-136/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace VariableAndDataTypes_136
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNum = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNum);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number + 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            char randomLetter = 'x';
+            char questionMark = '\u2103';
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+        }
+    }
+}
