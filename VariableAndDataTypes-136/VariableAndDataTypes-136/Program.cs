4502f264973efbd2c5674d893d393a89c74f109a
r3bunker <r3bunker@gmail.com>
2020-11-18 17:40:18 -0700

    Added saved files

--- VariableAndDataTypes-136/VariableAndDataTypes-136/Program.cs
+++ VariableAndDataTypes-136/VariableAndDataTypes-136/Program.cs
@@ -20,11 +20,26 @@ namespace VariableAndDataTypes_136
             //Console.WriteLine("Your favorite number + 5 is: " + total);
             //Console.ReadLine();
 
+
+            // Data Types //
             bool isStudying = false;
             byte hoursWorked = 42;
             sbyte currentTemp = -23;
             char randomLetter = 'x';
             char questionMark = '\u2103';
+            decimal moneyInBank = 100.5m;
+            double heightInCentimeters = 211.231241;
+            float secondsLeft = 2.654f;
+            short temperaturesOnMars = -341;
+            string myName = "Ryan";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+
 
             Console.WriteLine(questionMark);
             Console.ReadLine();
