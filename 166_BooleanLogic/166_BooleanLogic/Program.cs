3237fe9854240509c31c03cc2bd966855829c714
r3bunker <r3bunker@gmail.com>
2020-11-19 11:00:00 -0700

    Step 166

---
+++ 166_BooleanLogic/166_BooleanLogic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _166_BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // And operator
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            // Or operator
+            Console.WriteLine(true || false);
+            Console.WriteLine(true || true);
+
+            // Equaliy operator
+            Console.WriteLine(true == false);
+            Console.WriteLine(true == true);
+
+            // Not operator
+            Console.WriteLine(true != false);
+            Console.WriteLine(true != true);
+
+            // Xor operator
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(true ^ true);
+
+            Console.ReadLine();
+        }
+    }
+}
