507ac14d2e0f0eb27c360327c014b3eaaaeaed36
r3bunker <r3bunker@gmail.com>
2020-11-18 18:00:25 -0700

    Step 147
    math practice

---
+++ 147_MathChallenge/147_MathChallenge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace _147_MathChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Ask for first number
+            Console.WriteLine("Pick any number: ");
+            string firstAnswer = Console.ReadLine();
+            int num1 = Convert.ToInt32(firstAnswer); //converts string to int
+
+
+            // Ask for second number
+            Console.WriteLine("Pick another number: ");
+            string secondAnswer = Console.ReadLine();
+            int num2 = Convert.ToInt32(secondAnswer); //converts string to int
+
+            // Addition
+            int addition = num1 + num2;
+            Console.WriteLine(addition);
+
+            // Subtraction
+            int subtraction = num1 - num2;
+            Console.WriteLine(subtraction);
+
+            // Multiplication
+            int multiply = num1 * num2;
+            Console.WriteLine(multiply);
+
+            // Division
+            int divide = num1 / num2;
+            Console.WriteLine(divide);
+
+            Console.ReadLine();
+
+            // String concatenation
+            string firstName = "Ryan";
+            string lastName = "Bunker";
+            Console.WriteLine(firstName + lastName);
+            Console.ReadLine();
+
+        }
+    }
+}
