25c3aa3becb6dc5c3b2887854692c1dd1140aa3e
r3bunker <r3bunker@gmail.com>
2020-11-19 03:32:54 -0700

    Drill 158

---
+++ 158_MathConsoleApp/158_MathConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace _158_MathConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Ask user for number to multiply by 50
+            Console.WriteLine("Enter a number to multiply: ");
+            int times50 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(times50 * 50);
+            Console.ReadLine();
+
+            // Ask user for number to add by 50
+            Console.WriteLine("Enter a number to add: ");
+            int add25 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(add25 + 50);
+            Console.ReadLine();
+
+            // Ask user for number to divide by 12.5
+            Console.WriteLine("Enter a number to divide: ");
+            int divide12 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(divide12 / 12.5);
+            Console.ReadLine();
+
+            // Ask user for number to see if it's greater than 50
+            Console.WriteLine("Enter a number to see if it's greater than 50: ");
+            int greater50 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(greater50 > 50);
+            Console.ReadLine();
+
+            // Ask user for number to divide by 7 for remainder
+            Console.WriteLine("Enter a number to find the remainder: ");
+            int remainder7 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(remainder7 % 7);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
