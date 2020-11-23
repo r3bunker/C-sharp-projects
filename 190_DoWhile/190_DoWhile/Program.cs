e2cfe266096a15da17863a12375a496dd456a20f
r3bunker <r3bunker@gmail.com>
2020-11-23 14:11:27 -0700

    Drill 190

---
+++ 190_DoWhile/190_DoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _190_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Value of i: {0}", i);
+                i++;
+
+            }
+            while(i<10);
+            Console.ReadLine();
+        }
+    }
+}
