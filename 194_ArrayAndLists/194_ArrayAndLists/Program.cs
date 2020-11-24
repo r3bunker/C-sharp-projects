5a1ebacf44be1e1c62ceaa1b75dcf9de8262f400
r3bunker <r3bunker@gmail.com>
2020-11-24 04:11:08 -0700

    Step 194

---
+++ 194_ArrayAndLists/194_ArrayAndLists/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        List<int> intList = new List<int>();
+        intList.Add(5);
+        intList.Add(10);
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+ 
+
+        //// Example 1
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //// Example 2
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        //// Example 3
+        //int[] numArray2 = { 5, 2, 10, 200, 5000 };
+
+
+
+        //Console.WriteLine(numArray[3]);
+        //Console.WriteLine(numArray1[3]);
+        //Console.WriteLine(numArray2[3]);
+        //Console.ReadLine();
+    }
+}
+
