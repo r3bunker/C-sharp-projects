1ab20d3085917c28e1f8be5067149a30483b8ec6
r3bunker <r3bunker@gmail.com>
2020-12-02 15:26:25 -0700

    Drill 223

---
+++ 223_MethodsDrill/223_MethodsDrill/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _223_MethodsDrill
+{
+    public class Operation
+    {
+        public int getMultiply(int input, int value)
+        {
+            return input * value;
+        }
+
+        public int getDivide(int input, int value)
+        {
+            return input / value;
+        }
+
+        public int getAdd(int input, int value)
+        {
+            return input + value;
+        }
+ 
+    }
+}
---
+++ 223_MethodsDrill/223_MethodsDrill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _223_MethodsDrill
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // Ask user for number 
+            Console.WriteLine("Enter in a number: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Call methods
+            var operationObject = new Operation();
+            var result = operationObject.getMultiply(userNum, 5);
+            result = operationObject.getDivide(result, 2);
+            result = operationObject.getAdd(result, 10);
+
+            // Print result 
+            Console.WriteLine("Results: " + result);
+            Console.ReadLine();
+
+            
+        }
+
+        
+    }
+}
