47a7ea7695e28b7b842137983cdce347cd5d2d07
r3bunker <r3bunker@gmail.com>
2020-12-02 15:28:24 -0700

    Drill 227

---
+++ 227_OptionalParameter/227_OptionalParameter/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _227_OptionalParameter
+{
+    public class Math
+    {
+        public int getAdd(int input1, int input2 = 5)
+        {
+            return input1 + input2;
+        }
+    }
+}
---
+++ 227_OptionalParameter/227_OptionalParameter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _227_OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate method 
+            var addition = new Math();
+
+            // Ask user for input1
+            Console.WriteLine("Enter in a number");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask user for input2
+            Console.WriteLine("Enter in another number (optional)");
+            int input2;
+            int result;
+            if (int.TryParse(Console.ReadLine(), out input2))
+            {
+                result = addition.getAdd(input1, input2);
+            }
+            else
+            {
+                result = addition.getAdd(input1);
+            }
+
+            Console.WriteLine("Total sum: " + result);
+            Console.ReadLine();
+        }
+    }
+}
