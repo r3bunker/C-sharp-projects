6e0ed8f34738057f4174bbc6ff926b5dad9e6039
r3bunker <r3bunker@gmail.com>
2020-12-02 15:27:29 -0700

    Drill 226

---
+++ 226_MethodOverload/226_MethodOverload/MethodOverload.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _226_MethodOverload
+{
+    class MethodOverload
+    {
+        public int Operation(int input)
+        {
+            int addResult = input + 10;
+            return addResult;
+        }
+
+        public int Operation(decimal input)
+        {
+            decimal decResult = input / 2;
+            return (int)decResult;
+        }
+
+        public int Operation(string input)
+        {
+            int stringResult = Convert.ToInt32(input) - 5;
+            return stringResult;
+        }
+    }
+}
---
+++ 226_MethodOverload/226_MethodOverload/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _226_MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Instantiate class
+            var operationObject = new MethodOverload();
+            var addResults = operationObject.Operation(6);
+            var decResults = operationObject.Operation(10.5m);
+            var stringResults = operationObject.Operation("2");
+
+
+            Console.WriteLine(addResults);
+            Console.WriteLine(decResults);
+            Console.WriteLine(stringResults);
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
