3f7e7e45c874377b21bd8af3f653da26e5ee2ec3
r3bunker <r3bunker@gmail.com>
2020-12-02 15:29:09 -0700

    Drill 228

---
+++ 228_VoidMethod/228_VoidMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _228_VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Method
+            Void product = new Void();
+
+            Void.computeProduct(num2: 5, num1: 10);
+
+            
+            Console.ReadLine();
+        }
+      
+    }
+}
---
+++ 228_VoidMethod/228_VoidMethod/Void.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _228_VoidMethod
+{
+    class Void
+    {
+        public static void computeProduct(int num1, int num2)
+        {
+            int product = num1 * 10;
+            Console.WriteLine(num2);
+            return;
+
+
+        }
+    }
+}
