40c9e6ca561ef41aa5915a2d9289c093e57b804b
r3bunker <r3bunker@gmail.com>
2020-12-03 16:41:08 -0700

    Drill 231

---
+++ 231_ClassDrill/231_ClassDrill/Output.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _231_ClassDrill
+{
+    class Output
+    {
+        public void outputInt(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine("Result: " + num1 + " / 2 = " + result);
+            return;
+        }
+        public void outputInt(out int x)
+        {
+            x = 10;
+            x *= x;
+        }
+        public static int sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ 231_ClassDrill/231_ClassDrill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _231_ClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate class
+            var outputObject = new Output();
+
+            // Ask user for number
+            Console.WriteLine("Enter a number: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Call the method
+            outputObject.outputInt(userNum);
+            outputObject.outputInt(out int x);
+
+
+            Console.WriteLine("Overload Output: {0}", x);
+
+
+            Console.ReadLine();
+        }
+    }
+}
