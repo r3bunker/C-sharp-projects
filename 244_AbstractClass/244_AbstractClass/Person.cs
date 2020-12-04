8d5d04841f34cfc273b9a501e886f90d438334e1
r3bunker <r3bunker@gmail.com>
2020-12-03 17:46:19 -0700

    Drill 244

---
+++ 244_AbstractClass/244_AbstractClass/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _244_AbstractClass
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }  
+        public string LastName { get; set; }
+
+        public virtual void sayName()
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine(FullName);
+        }
+
+    }
+}
---
+++ 244_AbstractClass/244_AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _244_AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            employee.sayName();
+
+            Console.ReadLine();
+        }
+    }
+}
