00d4b7e05205c8652764b744bf593751eede5fa6
r3bunker <r3bunker@gmail.com>
2020-12-03 20:36:39 -0700

    Drill 252

---
+++ 252_OverloadOperator/252_OverloadOperator/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _252_OverloadOperator
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.ID == employee2.ID)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator !=(Employee employee, Employee employee1)
+        {
+            return employee.ID != employee1.ID;
+        }
+        
+    }
+}
+
---
+++ 252_OverloadOperator/252_OverloadOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _252_OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.Name = "Ryan";
+            employee.ID = 60231;
+
+            Employee employee1 = new Employee();
+
+            employee1.Name = "Jake";
+            employee1.ID = 60232;
+
+            Console.ReadLine();
+        }
+    }
+}
