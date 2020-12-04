98c83273daefaf6eba8292020180d11bfafaf448
r3bunker <r3bunker@gmail.com>
2020-12-04 16:25:25 -0700

    Drill 261

---
+++ 261_EnumChallenge/261_EnumChallenge/Days.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _261_EnumChallenge
+{
+    public class Days
+    {
+        
+        // Create enum of the days of the week
+        public enum DaysOfWeek
+        {
+            Monday=1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ 261_EnumChallenge/261_EnumChallenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _261_EnumChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for the day of the week
+            Console.WriteLine("What day of the week is it?");
+            try
+            {
+            Days.DaysOfWeek currentDay = (Days.DaysOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+
+            Console.WriteLine(currentDay);
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+
+        }
+        
+    }
+}
