9ebfec79961cfffd22b9bef56f21c4390fca4604
r3bunker <r3bunker@gmail.com>
2020-12-04 16:49:27 -0700

    Drill 267

---
+++ 267_Struct/267_Struct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _267_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 10.5m;
+
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+
+
+        }
+    }
+}
