028596e174dd88925e21d759d336c0519415c5d3
r3bunker <r3bunker@gmail.com>
2020-12-04 16:51:06 -0700

    Drill 255
    Having trouble iterating through my lists.

---
+++ 255_GenericType/255_GenericType/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _255_GenericType
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+    }
+}
---
+++ 255_GenericType/255_GenericType/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _255_GenericType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instantiate List type string
+            Employee<string> employees = new Employee<string>
+            {
+                Things = new List<string>()
+            };
+            employees.Things.Add("Josh");
+            employees.Things.Add("Jake");
+            employees.Things.Add("Ashley");
+            employees.Things.Add("Bill");
+
+            
+            // Instantiate List type int
+            Employee<int> employeeID = new Employee<int>
+            {
+                Things = new List<int>()
+            };
+            employeeID.Things.Add(3452);
+            employeeID.Things.Add(5643);
+            employeeID.Things.Add(5948);
+            employeeID.Things.Add(8348);
+
+
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+
+
+
+            Console.ReadLine();
+
+            
+            
+
+        }
+    }
+}
