5519160141bc4c127a0604d0f7aa14e5c9c4937c
r3bunker <r3bunker@gmail.com>
2020-12-03 17:09:30 -0700

    Drill 236

---
+++ 236_ClassInheritance/236_ClassInheritance/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _236_ClassInheritance
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ 236_ClassInheritance/236_ClassInheritance/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _236_ClassInheritance
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: [" + FullName + "]");
+        }
+    }
+}
