b8851221b92d5eaa6f91dad30e239a17101e9062
r3bunker <r3bunker@gmail.com>
2020-12-07 13:42:26 -0700

    Drill 297

---
+++ 297_LogFile/297_LogFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _297_LogFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for number
+            Console.WriteLine("Please enter any number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\RyanB\Logs\userNum.txt", true))
+            {
+                file.WriteLine(userNum);
+            }
+
+            string text = File.ReadAllText(@"C:\Users\RyanB\Logs\userNum.txt");
+
+            Console.ReadLine();
+
+        }
+    }
+}
