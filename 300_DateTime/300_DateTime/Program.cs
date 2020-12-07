75693d3c97873f8b8d30b71c44aeec7c444cf0ff
r3bunker <r3bunker@gmail.com>
2020-12-07 13:57:07 -0700

    Drill 300

---
+++ 300_DateTime/300_DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _300_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Current time
+            Console.WriteLine("Current time: {0}", DateTime.Now);
+            Console.ReadLine();
+
+            // Ask user for number
+            Console.WriteLine("Please enter a number of hours.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Exact time in X hours
+            DateTime currentTime = DateTime.Now;
+            DateTime futureTime = currentTime.AddHours(userNum);
+            Console.WriteLine("\nThe time will be: {0} in {1} hours from now.", futureTime, userNum);
+            Console.ReadLine();
+        }
+    }
+}
