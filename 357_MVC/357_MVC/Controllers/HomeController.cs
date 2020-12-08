b476fcf579e3714e5fff0101c08a13f1d089037a
r3bunker <r3bunker@gmail.com>
2020-12-08 10:36:04 -0700

    Step 357

---
+++ 357_MVC/357_MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using _357_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _357_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.id = 1;
+            user.FirstName = "Ryan";
+            user.LastName = "Bunker";
+            user.Age = 28;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            throw new Exception("Invalid Page");
+
+            return View();
+        }
+
+        public ActionResult Contact(int id=0)
+        {
+            ViewBag.Message = id;
+
+            return View();
+        }
+    }
+}
